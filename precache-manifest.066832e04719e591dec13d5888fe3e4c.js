0fcc46f4aea40fb24c8bb513877df2b6f1d05c96
Drewis Dorner <drewis@dorner-it.de>
2020-09-17 09:48:34 +0200

    Updates

---
+++ precache-manifest.066832e04719e591dec13d5888fe3e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e24b25e3e3eb3c9d77aead1dc1a629",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "bfaa7a32fa3e15f8d20e",
+    "url": "/react-game-of-life/static/js/2.b095853f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game-of-life/static/js/2.b095853f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc692f1e6293931cea8",
+    "url": "/react-game-of-life/static/js/main.05ca213f.chunk.js"
+  },
+  {
+    "revision": "56d0628c06fd0267653b",
+    "url": "/react-game-of-life/static/js/runtime-main.a4e5bf91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e094d4d2ec712cbdef0db2195a8499bd.js"
+  "/react-game-of-life/precache-manifest.066832e04719e591dec13d5888fe3e4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-game-of-life/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
